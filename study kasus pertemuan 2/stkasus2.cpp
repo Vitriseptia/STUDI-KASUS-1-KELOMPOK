bd2bd0f85e1837681ac069a9a441e50289c52eda
Vitriseptia <vitri2100018059@webmail.uad.ac.id>
2022-04-02 08:50:42 -1200

    file baru

--- study kasus pertemuan 2/stkasus2.cpp
+++ study kasus pertemuan 2/stkasus2.cpp
@@ -4,11 +4,26 @@ using namespace std;
 class developer{
 	public : 
 		void daftar();
+		void pokok();
+		void kerjaan();
 		void proses();
-		void pengalaman();
 		void output();
 	private : 
-		int dftr,kerja,gaji,ga,lama,posisi;
-		float pl,p2;
-		string nama;
+		int pekerjaan, dftr[2], lama[2],posisi[2];
+		double leader[2], gaji[2],thn[2], kurangan[2], gbulan[2], total[2],pl, tambahan[2];
+		string nama,p2, kr[10];
 };
+
+void developer::daftar(){
+
+	cout<<"Masukan nama anda : "; getline(cin,nama);cout<<endl; 
+	cout<<"Bidang pekerjaan"<<endl;
+	cout<<"	1. Express JS"<<endl;
+	cout<<"	2. Django python"<<endl;
+	cout<<"	3. Rails ruby"<<endl;
+	cout<<"	4. Laravel PHP"<<endl;
+	cout<<"	5. Spring Java"<<endl;
+	cout<<"	6. Angular JS"<<endl;
+	cout<<"Nomer bidang yang akan diambil? : "; cin>>dftr[0];
+	cout<<"Berapa lama pengalaman anda di bidang tersebut?(tahun) : "; cin>>thn[0];	
+}
