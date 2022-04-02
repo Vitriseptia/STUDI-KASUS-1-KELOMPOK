8132cf5553f7bc0de7b154701cc8c36241a71b87
DamarBagas <damar2100018056@webmail.uad.ac.id>
2022-04-02 09:16:19 +0900

    update code

--- study kasus pertemuan 2/stkasus2.cpp
+++ study kasus pertemuan 2/stkasus2.cpp
@@ -27,3 +27,64 @@ void developer::daftar(){
 	cout<<"Nomer bidang yang akan diambil? : "; cin>>dftr[0];
 	cout<<"Berapa lama pengalaman anda di bidang tersebut?(tahun) : "; cin>>thn[0];	
 }
+
+void developer::kerjaan(){
+	if(thn[0]>=2.5){
+		cout<<"Apakah anda ingin melamar 2 pekerjaan?(y/n) : "; cin>>p2;}
+		if(p2=="y"){
+			cout<<"Bidang kedua yang akan diambil? : "; cin>>dftr[1];
+			cout<<endl;
+			cout<<"Berapa lama pengalaman anda di bidang tersebut? : "; cin>>thn[1];
+				if(thn[1]<2.5){
+				dftr[1]=0;
+				cout<<"Anda hanya dapat mendaftar satu perkerjaan,yaitu "<<dftr<<endl;
+				}
+			}
+
+
+	cout<<"Berapa lama anda menyelesaikan pekerjaan  bidang 1 (bulan) : "; cin>>lama[0];
+	cout<<"posisi\n 1)leader\n 2)anggota\n : "; cin>>posisi[0];
+	if(thn[1]>=2.5){
+		pekerjaan=2;
+		cout<<"Berapa lama anda menyelesaikan pekerjaan bidang 2? ";cin>>lama[1];
+	cout<<"posisibidang 2: \n 1)leader\n 2)anggota : "; cin>>posisi[1];
+	}
+}
+
+void developer::pokok(){
+	for(int i=0; i<2;i++){
+	switch(dftr[i]){
+			case 1 : {
+				kr[i]="Express JS";
+				gaji[i]=15000000;
+				break;
+			}
+			case 2 :{
+				kr[i]="Django pyton";
+				gaji[i]=13000000;
+				break;
+			}
+			case 3 :{
+				kr[i]="Rails ruby";
+				gaji[i]=8000000;
+				break;
+			}
+			case 4 :{
+				kr[i]="Laravel PHP";
+				gaji[i]=18000000;
+				break;
+			}
+			case 5 :{
+				kr[i]="Spring Java";
+				gaji[i]=8000000;
+				break;
+			}
+			case 6 :{
+				kr[i]="Angular JS";
+				gaji[i]=15000000;
+				break;
+			}
+		}
+}
+}
+
