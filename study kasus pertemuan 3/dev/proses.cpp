cdc36d780de256c65abc5c324e62f8c887998e9f
Vitriseptia <vitri2100018059@webmail.uad.ac.id>
2022-04-08 09:50:06 -1200

    file baru

--- study kasus pertemuan 3/dev/proses.cpp
+++ study kasus pertemuan 3/dev/proses.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <fstream>
 #include ".../library/proses.h"
 
 int main(){
 	proses proses;
 	proses.cetak();
+	cout<<"Anda siap untuk menjadi developer proses \n";
 	return 0;
 }
