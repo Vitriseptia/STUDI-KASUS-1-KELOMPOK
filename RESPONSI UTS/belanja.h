da378ec78d8c806616fb4f4d4588858c5bfcc9ca
Vitriseptia <vitri2100018059@webmail.uad.ac.id>
2022-05-21 14:54:50 -1200

    vitri septia

--- RESPONSI UTS/belanja.h
+++ RESPONSI UTS/belanja.h
@@ -27,34 +27,34 @@ long belanja,total;
 };
 void shopee::input(){
   cout<<"|==================================|"<<endl;
-	cout<<"|              SHOPEE              |"<<endl;
-	cout<<"|==================================|"<<endl;
-	cout<<"| Masukkan Akun Anda                "<<endl;
-	cout<<"| Username	: "; cin>>un;
-	cout<<"| Password	: "; cin>>ps;
+  cout<<"|              SHOPEE              |"<<endl;
   cout<<"|==================================|"<<endl;
-	cout<<"|      Masukkan identitas anda     |"<<endl; 
-	cout<<"|==================================|"<<endl;
+  cout<<"| Masukkan Akun Anda                "<<endl;
+  cout<<"| Username : "; cin>>un;
+  cout<<"| Password : "; cin>>ps;
+  cout<<"|==================================|"<<endl;
+  cout<<"|      Masukkan identitas anda     |"<<endl; 
+  cout<<"|==================================|"<<endl;
   cin.ignore();
-	cout<<"| Nama lengkap   : "; getline(cin,nama); 
-	cout<<"| Alamat lengkap : "; getline(cin,alamat);
-	cout<<"| No HP          : +62"; cin>>no;
+  cout<<"| Nama lengkap   : "; getline(cin,nama); 
+  cout<<"| Alamat lengkap : "; getline(cin,alamat);
+  cout<<"| No HP          : +62"; cin>>no;
   cout<<"| Saldo Shopeepay: "; cin>>saldo;
   cout<<"|----------------------------------|"<<endl;
   do
     {
       cout<<"\n|==========================================|"<<endl;
-  		cout<<"|             SELAMAT BERBELANJA           |"<<endl;
-  		cout<<"|==========================================|"<<endl;
-  		cout<<"|1.Fashion                                 |"<<endl;
+      cout<<"|             SELAMAT BERBELANJA           |"<<endl;
+      cout<<"|==========================================|"<<endl;
+      cout<<"|1.Fashion                                 |"<<endl;
       cout<<"|  a.Kaos               Rp.  40.000        |"<<endl;
       cout<<"|  b.Kemeja             Rp.  50.000        |"<<endl;
       cout<<"|  c.Celana             Rp.  100.000       |"<<endl;
-  		cout<<"|  b.Rok                Rp.  30.000        |"<<endl;
-  		cout<<"|2.Elektronik                              |"<<endl;
-  	  cout<<"|  a.Smartphone         Rp.  500.000       |"<<endl;
+      cout<<"|  b.Rok                Rp.  30.000        |"<<endl;
+      cout<<"|2.Elektronik                              |"<<endl;
+      cout<<"|  a.Smartphone         Rp.  500.000       |"<<endl;
       cout<<"|  b.Laptop             Rp.  2.000.000     |"<<endl;
-  		cout<<"|==========================================|"<<endl;
+      cout<<"|==========================================|"<<endl;
       cout<<"|berapa banyak barang pilihan : "; cin>>menu;
       cout<<endl;
       for(int i=0; i<menu; i++)
@@ -69,18 +69,18 @@ void shopee::input(){
     }
     while (lagi!= 't');
     cout<<endl;
-  cout<<"|==============================|"<<endl;
-  cout<<"|Jasa pengiriman yang tersedia |"<<endl;
-  cout<<"|------------------------------|"<<endl;
-  cout<<"|1. j&t                        |"<<endl;
-  cout<<"|2. jne                        |"<<endl;
-  cout<<"|3. Reguler                    |"<<endl;
-  cout<<"|4. Kargo                      |"<<endl;
-  cout<<"|==============================|"<<endl;
+  cout<<"=============================="<<endl;
+  cout<<"Jasa pengiriman yang tersedia             "<<endl;
+  cout<<" 1. j&t\n 2. jne\n 3. Reguler\n 4. Kargo  "<<endl;
+  cout<<"------------------------------"<<endl;
   cout<<"jasa pengiriman yang akan digunakan ? "; cin>>jasa;
+
+  
 }
 
 void shopee::proses(){
+
+  
 //bagian hitung harga barang//
   belanja=0;
    for(int i=0; i<menu; i++){
@@ -88,8 +88,9 @@ void shopee::proses(){
     belanja=belanja+hargaitem[i];
    }
   
-//Bagian jasa pengiriman//
-	switch(jasa){
+    //Bagian jasa pengiriman//
+
+  switch(jasa){
     case 1:
       {
         kurir="j&t";
@@ -123,32 +124,32 @@ void shopee::proses(){
 
   //bagian voucher
  if(belanja>5000000){
-    cout<<"\n|=================================|"<<endl;
-    cout<<"|Voucher tersedia                 |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
-    cout<<"|1. Diskon 10.000 min.Blj 100000  |"<<endl;
-    cout<<"|2. Diskon 7% min.Blj 1000000     |"<<endl;
-    cout<<"|3. Diskon 5% min.Blj 3000000     |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
+    cout<<"\n================================="<<endl;
+    cout<<"Voucher tersedia"<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout<<"1. Diskon 10.000 min.Blj 100000"<<endl;
+    cout<<"2. Diskon 7% min.Blj 1000000"<<endl;
+    cout<<"3. Diskon 5% min.Blj 3000000 "<<endl;
+    cout<<"----------------------------------"<<endl;
     cout<<"Pilih salah satu voucher : "; cin>>voucherr;
   }
 
  else if(belanja>1000000){
-    cout<<"\n|=================================|"<<endl;
-    cout<<"|Voucher tersedia                 |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
-    cout<<"|1. Diskon 10.000 min.Blj 100000  |"<<endl;
-    cout<<"|2. Diskon 7% min.Blj 1000000     |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
+    cout<<"\n================================="<<endl;
+    cout<<"Voucher tersedia"<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout<<"1. Diskon 10.000 min.Blj 100000"<<endl;
+    cout<<"2. Diskon 7% min.Blj 1000000 "<<endl;
+    cout<<"----------------------------------"<<endl;
     cout<<"Pilih salah satu voucher : "; cin>>voucherr;
  }
 
   else if (belanja>100000){
-    cout<<"\n|=================================|"<<endl;
-    cout<<"|Voucher tersedia                 |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
-    cout<<"|1. Diskon 10.000 min.Blj 100000  |"<<endl;
-    cout<<"|---------------------------------|"<<endl;
+    cout<<"\n================================="<<endl;
+    cout<<"Voucher tersedia"<<endl;
+    cout<<"----------------------------------"<<endl;
+    cout<<"1. Diskon 10.000 min.Blj 100000"<<endl;
+    cout<<"----------------------------------"<<endl;
     cout<<"Pilih salah satu voucher : "; cin>>voucherr;
   }
 
@@ -174,8 +175,7 @@ switch(voucherr){
       break;
     }
 }
-//Bagian metode pembayaran
-pay:
+pay://Bagian metode pembayaran
   cout<<"\n|===================|"<<endl;  
   cout<<"|Metode pembayaran  |"<<endl;
   cout<<"|-------------------|"<<endl;  
@@ -185,22 +185,23 @@ pay:
   cout<<"|4. Transfer bank   |"<<endl;
   cout<<"|-------------------|"<<endl; 
   cout<<"Metode pembayaran yang akan digunakan ? "; cin>>cara;
-	switch(cara){
+  switch(cara){
     case 1: 
       {
         metode="Shopeepay";
         if(saldo<belanja){
-          cout<<"Shopeepay anda tidak cukup. Silahkan pilih metode pembayaran yang lain"<<endl;
+          cout<<"Shopeepay anda tidak cukup. Silahkan                           pilih metode pembayaran yang lain"<<endl;
           goto pay;
             }
             else{
-            cout<<"\n|==============================|"<<endl;
-            cout<<"|Voucher gratis ongkir 10 ribu |"<<endl;
-            cout<<"|==============================|"<<endl;
-            cout<<"Gunakan voucher? (y/t) "; cin>>diskonongkir;
+            cout<<"\n=============================="            <<endl;
+            cout<<"Voucher gratis ongkir 10 ribu "                  <<endl;
+            cout<<"Gunakan voucher? (y/t) ";           cin>>diskonongkir;
+            cout<<"-------------------------------"<<endl;
             if(diskonongkir='y'){
               gratong=10000;
             }
+            
               }
         break;
       } 
@@ -239,10 +240,12 @@ else{
   bproteksi=0;
 }
 
+
+
 //bagian total akhir 
 total=belanja+ongkir-gratong-voucher+bproteksi;
  
-}
+  }
 
    
 
